577491b344c5a107b0a9d4e5decc4bb490bc55d2
Luis Weich <luis.weich@study.thws.de>
2024-11-18 14:13:54 +0100

    add jpa entities

---
+++ src/main/java/de/thws/milu/infrastructure/persistence/entity/JpaBoard.java
@@ -0,0 +1,76 @@
+package de.thws.milu.infrastructure.persistence.entity;
+
+import de.thws.milu.domain.model.Board;
+import jakarta.persistence.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "board")
+public class JpaBoard implements Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(mappedBy = "parentBoard", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<JpaTodo> todos;
+
+    public JpaBoard() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JpaBoard that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public List<JpaTodo> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(List<JpaTodo> todos) {
+        this.todos = todos;
+    }
+}
