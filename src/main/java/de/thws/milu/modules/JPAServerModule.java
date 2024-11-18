c3dfd7f49edc0f576aea60bd7f16d0a45c6453da
Luis Weich <luis.weich@study.thws.de>
2024-11-18 17:51:37 +0100

    rename ServerModule to JPAServerModule

--- src/main/java/de/thws/milu/modules/JPAServerModule.java
+++ src/main/java/de/thws/milu/modules/JPAServerModule.java
@@ -7,13 +7,21 @@ import de.thws.milu.domain.repository.TodoRepository;
 import de.thws.milu.infrastructure.persistence.jpa.JpaAccountRepository;
 import de.thws.milu.infrastructure.persistence.jpa.JpaBoardRepository;
 import de.thws.milu.infrastructure.persistence.jpa.JpaTodoRepository;
+import jakarta.inject.Provider;
+import jakarta.persistence.EntityManagerFactory;
+import java.util.Objects;
 
-public class ServerModule extends AbstractModule {
+public class JPAServerModule extends AbstractModule {
 
-    public ServerModule() {}
+    private final Provider<EntityManagerFactory> entityManagerFactory;
+
+    public JPAServerModule(Provider<EntityManagerFactory> entityManagerFactory) {
+        this.entityManagerFactory = Objects.requireNonNull(entityManagerFactory);
+    }
 
     @Override
     protected void configure() {
+        bind(EntityManagerFactory.class).toProvider(entityManagerFactory);
         bind(AccountRepository.class).to(JpaAccountRepository.class);
         bind(BoardRepository.class).to(JpaBoardRepository.class);
         bind(TodoRepository.class).to(JpaTodoRepository.class);
