d4068d0874787bc4780dbee2034b3cc997ac54e8
Luis Weich <luis.weich@study.thws.de>
2024-11-18 18:28:12 +0100

    update AccountResource

--- src/main/java/de/thws/milu/resources/AccountResource.java
+++ src/main/java/de/thws/milu/resources/AccountResource.java
@@ -1,10 +1,13 @@
 package de.thws.milu.resources;
 
+import de.thws.milu.application.service.AccountService;
+import de.thws.milu.domain.model.Account;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.util.List;
 
 @Singleton
 @Path("/account")
@@ -12,11 +15,18 @@ import jakarta.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class AccountResource {
 
+    private final AccountService accountService;
+
     @Inject
-    public AccountResource() {}
+    public AccountResource(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @GET
     public Response getAccounts() {
-        return Response.ok().build();
+
+        List<Account> accounts = accountService.getAccounts();
+
+        return Response.ok(accounts).build();
     }
 }
