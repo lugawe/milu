1e23181d9c067edc71405ba7e1425a4950c2bd2c
Luis Weich <luis.weich@study.thws.de>
2024-11-18 18:01:59 +0100

    add MiluServerSetup

---
+++ src/main/java/de/thws/milu/setup/MiluServerSetup.java
@@ -0,0 +1,30 @@
+package de.thws.milu.setup;
+
+import com.google.inject.AbstractModule;
+import de.thws.milu.MiluServerConfiguration;
+import de.thws.milu.modules.JPAServerModule;
+import io.dropwizard.core.ConfiguredBundle;
+import io.dropwizard.core.setup.Bootstrap;
+import io.dropwizard.core.setup.Environment;
+
+public class MiluServerSetup extends AbstractModule implements ConfiguredBundle<MiluServerConfiguration> {
+
+    private final MiluScanningHibernateBundle hibernateBundle = new MiluScanningHibernateBundle();
+
+    public MiluServerSetup() {}
+
+    @Override
+    protected void configure() {
+        install(new JPAServerModule(hibernateBundle::getSessionFactory));
+    }
+
+    @Override
+    public void initialize(Bootstrap<?> bootstrap) {
+        hibernateBundle.initialize(bootstrap);
+    }
+
+    @Override
+    public void run(MiluServerConfiguration configuration, Environment environment) throws Exception {
+        hibernateBundle.run(configuration, environment);
+    }
+}
