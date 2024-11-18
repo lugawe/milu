06390faf663d0d0d8810b083cd5ced8b51d9206c
Luis Weich <luis.weich@study.thws.de>
2024-11-18 23:27:38 +0100

    AccountService update

--- src/main/java/de/thws/milu/application/service/AccountService.java
+++ src/main/java/de/thws/milu/application/service/AccountService.java
@@ -4,6 +4,8 @@ import de.thws.milu.domain.model.Account;
 import de.thws.milu.domain.repository.AccountRepository;
 import jakarta.inject.Inject;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class AccountService {
 
@@ -14,7 +16,19 @@ public class AccountService {
         this.accountRepository = accountRepository;
     }
 
-    public List<Account> getAccounts() {
+    public Optional<Account> getById(UUID id) {
+        return accountRepository.getById(id);
+    }
+
+    public List<Account> getAll() {
         return accountRepository.getAll().stream().map(a -> (Account) a).toList();
+    }
+
+    public void save(Account account) {
+        accountRepository.save(account);
+    }
+
+    public void deleteById(UUID id) {
+        accountRepository.deleteById(id);
     }
 }
