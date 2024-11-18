db68b89960ab0b8c03abfbd4627e1c3e8a7370f0
Luis Weich <luis.weich@study.thws.de>
2024-11-18 19:08:18 +0100

    JPAServerModule rename to HibernateServerModule

--- src/main/java/de/thws/milu/modules/HibernateServerModule.java
+++ src/main/java/de/thws/milu/modules/HibernateServerModule.java
@@ -4,24 +4,26 @@ import com.google.inject.AbstractModule;
 import de.thws.milu.domain.repository.AccountRepository;
 import de.thws.milu.domain.repository.BoardRepository;
 import de.thws.milu.domain.repository.TodoRepository;
+import de.thws.milu.infrastructure.persistence.hibernate.HibernateJpaFactory;
 import de.thws.milu.infrastructure.persistence.jpa.JpaAccountRepository;
 import de.thws.milu.infrastructure.persistence.jpa.JpaBoardRepository;
+import de.thws.milu.infrastructure.persistence.jpa.JpaFactory;
 import de.thws.milu.infrastructure.persistence.jpa.JpaTodoRepository;
 import jakarta.inject.Provider;
-import jakarta.persistence.EntityManagerFactory;
-import java.util.Objects;
+import org.hibernate.SessionFactory;
 
-public class JPAServerModule extends AbstractModule {
+public class HibernateServerModule extends AbstractModule {
 
-    private final Provider<EntityManagerFactory> entityManagerFactory;
+    private final Provider<SessionFactory> sessionFactoryProvider;
 
-    public JPAServerModule(Provider<EntityManagerFactory> entityManagerFactory) {
-        this.entityManagerFactory = Objects.requireNonNull(entityManagerFactory);
+    public HibernateServerModule(Provider<SessionFactory> sessionFactoryProvider) {
+        this.sessionFactoryProvider = sessionFactoryProvider;
     }
 
     @Override
     protected void configure() {
-        bind(EntityManagerFactory.class).toProvider(entityManagerFactory);
+        bind(SessionFactory.class).toProvider(sessionFactoryProvider);
+        bind(JpaFactory.class).to(HibernateJpaFactory.class);
         bind(AccountRepository.class).to(JpaAccountRepository.class);
         bind(BoardRepository.class).to(JpaBoardRepository.class);
         bind(TodoRepository.class).to(JpaTodoRepository.class);
