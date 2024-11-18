ad3f94928e73acba46d1ab5493ecd0bf9ff94506
Luis Weich <luis.weich@study.thws.de>
2024-11-18 14:25:12 +0100

    update ServerModule

--- src/main/java/de/thws/milu/modules/ServerModule.java
+++ src/main/java/de/thws/milu/modules/ServerModule.java
@@ -1,5 +1,21 @@
 package de.thws.milu.modules;
 
 import com.google.inject.AbstractModule;
+import de.thws.milu.domain.repository.AccountRepository;
+import de.thws.milu.domain.repository.BoardRepository;
+import de.thws.milu.domain.repository.TodoRepository;
+import de.thws.milu.infrastructure.persistence.JpaAccountRepository;
+import de.thws.milu.infrastructure.persistence.JpaBoardRepository;
+import de.thws.milu.infrastructure.persistence.JpaTodoRepository;
 
-public class ServerModule extends AbstractModule {}
+public class ServerModule extends AbstractModule {
+
+    public ServerModule() {}
+
+    @Override
+    protected void configure() {
+        bind(AccountRepository.class).to(JpaAccountRepository.class);
+        bind(BoardRepository.class).to(JpaBoardRepository.class);
+        bind(TodoRepository.class).to(JpaTodoRepository.class);
+    }
+}
