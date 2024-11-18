55dcc0ce78c9173f8fcd86f6d2b9f2c021e4ddbd
Luis Weich <luis.weich@study.thws.de>
2024-11-18 23:23:26 +0100

    add NoValuesAffectedException

---
+++ src/main/java/de/thws/milu/domain/exception/NoValuesAffectedException.java
@@ -0,0 +1,20 @@
+package de.thws.milu.domain.exception;
+
+public class NoValuesAffectedException extends RuntimeException {
+
+    public NoValuesAffectedException() {
+        super();
+    }
+
+    public NoValuesAffectedException(String message) {
+        super(message);
+    }
+
+    public NoValuesAffectedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoValuesAffectedException(Throwable t) {
+        super(t);
+    }
+}
