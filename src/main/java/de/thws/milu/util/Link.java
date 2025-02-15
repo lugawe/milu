6e87a0c723ee3c944cedf6efa5eb074996fe02f6
Michael <michael.winsauer@study.thws.de>
2025-02-15 13:57:00 +0100

    fix merge

--- src/main/java/de/thws/milu/util/Link.java
+++ src/main/java/de/thws/milu/util/Link.java
@@ -1,11 +1,37 @@
 package de.thws.milu.util;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Link {
+
+    @JsonProperty("rel")
     private String rel;
+
+    @JsonProperty("href")
     private String href;
+
+    public Link() {
+        // Default constructor needed for Jackson
+    }
 
     public Link(String rel, String href) {
         this.rel = rel;
+        this.href = href;
+    }
+
+    public String getRel() {
+        return rel;
+    }
+
+    public void setRel(String rel) {
+        this.rel = rel;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
         this.href = href;
     }
 }
--- src/main/java/de/thws/milu/util/Resource.java
+++ src/main/java/de/thws/milu/util/Resource.java
@@ -1,18 +1,41 @@
 package de.thws.milu.util;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Resource<T> {
+
+    @JsonProperty("data")
     private T data;
+
+    @JsonProperty("links")
     private List<Link> links = new ArrayList<>();
 
-    public Resource() {}
+    public Resource() {
+        // Default constructor needed for Jackson
+    }
 
     public Resource(T data) {
         this.data = data;
     }
 
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Link> links) {
+        this.links = links;
+    }
 
     public void addLink(String rel, String href) {
         this.links.add(new Link(rel, href));
